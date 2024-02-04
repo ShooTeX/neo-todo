18d492f39d41253c0401575338830ab336f119b5
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-04 23:42:21 +0100

    styling and stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./web/views/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Space Mono"', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 }
