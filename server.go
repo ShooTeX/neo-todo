ac5b6c7e832234aaa18973c4dff40e8e06c925c8
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-04 22:16:09 +0100

    templates

--- server.go
+++ server.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+// INFO: how do I make the template name safe?
+func Hello(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "World")
+}
+
 func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("web/views/*.html")),
+	}
 	e := echo.New()
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{Format: "method=${method}, uri=${uri}, status=${status}\n"}))
+	e.Renderer = t
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.GET("/hello", Hello)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
