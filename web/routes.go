a61e4ac04374daffb7d97c013ad75cd896ae2f92
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-06 20:34:49 +0100

    remove example

--- web/routes.go
+++ web/routes.go
@@ -11,11 +11,6 @@ func Index(c echo.Context) error {
 	return c.Render(http.StatusOK, "index", nil)
 }
 
-func Example(c echo.Context) error {
-	return c.Render(http.StatusOK, "example", "World")
-}
-
 func SetWebRoutes(e *echo.Echo) {
 	e.GET("/", Index)
-	e.GET("/example", Example)
 }
