cbd80fd2bea01695222094f4ab9ab7ab2fe6d9de
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-06 23:35:13 +0100

    styling :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ export default {
         'sans': ['"Space Mono"', ...defaultTheme.fontFamily.mono],
       },
     },
+    boxShadow: {
+      default: "8px 5px 0px 0px #000000",
+      sm: "2px 3px 0px 0px #000000",
+      lg: "10px 7px 0px 0px #000000",
+    }
   },
   plugins: [],
 }
