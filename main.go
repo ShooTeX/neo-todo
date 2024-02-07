3c1ddd75469ca7fff12442c08d6554dbc02eb203
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-08 00:18:25 +0100

    remove unused

--- main.go
+++ main.go
@@ -2,28 +2,12 @@ package main
 
 import (
 	_ "embed"
-	"html/template"
-	"io"
 	"os"
 
-	"github.com/labstack/echo/v4"
 	"github.com/shootex/neo-todo/db"
 	"github.com/shootex/neo-todo/handler"
 	"github.com/shootex/neo-todo/router"
-	"github.com/shootex/neo-todo/web/views"
 )
-
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-var t = &Template{
-	templates: template.Must(template.ParseFS(views.Resources, "*.html")),
-}
 
 func main() {
 	port := os.Getenv("PORT")
